03aa91e9f6259facaac36b02faec0a7ff7ef526e
natelia <natalia.jadwiga.gajewska@gmail.com>
2023-11-02 21:45:38 +0100

    first commit

---
+++ name_length.rb
@@ -0,0 +1,6 @@
+puts "what is your name?"
+name = gets.chomp
+puts "did you know that your name counts #{name.length} characters?"
+puts "what is your surname?"
+surname = gets.chomp
+puts "wow your surname counts #{surname.length} characters and your full name is #{surname.length + name.length} characters long!"
